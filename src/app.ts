95752ea74f1ca862175a5789ea9d3daede07ee2f
Jano Soler <jano.soler@xarxatecactiva.com>
2022-03-09 07:03:43 +0100

    "proyecto clean code"

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,16 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join( __dirname)));
 
 app.use("/", (req: express.Request, res: express.Response)=>{
-    res.sendFile(path.join(__dirname, "..",'public', 'index.html'));
+    res.sendFile(path.join(__dirname,'..','public', 'index.html'));
+})
+
+app.use("/winner", (req: express.Request, res: express.Response)=>{
+    res.sendFile(path.join(__dirname,'..','public', 'winner.html'));
+})
+
+app.use("/loser", (req: express.Request, res: express.Response)=>{
+    console.log(path.join(__dirname,'..','public', 'loser.html'));
+    res.sendFile(path.join(__dirname,'..','public', 'loser.html'));
 })
 
 app.listen(3000, ()=>{console.log("Wordle is listening at port 3000...")})
