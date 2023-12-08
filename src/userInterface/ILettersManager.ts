d31524850a4d953be943a663ce65ad6f568e761f
Mariajo <albiach33@gmail.com>
2023-12-08 12:22:57 +0100

    refactor: improve ILetterManager

--- src/userInterface/ILettersManager.ts
+++ src/userInterface/ILettersManager.ts
@@ -1,4 +1,6 @@
 export interface ILetterManagerInterface {
+    turn: number;
+    position: number;
     letter: string;
     setNewLetter(turn: number, position: number, letter: string): void;
     deleteLetter(turn: number, position: number): void;
