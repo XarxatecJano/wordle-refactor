fccf3a09385dd14c226e41a4913f1ec2d2980ba1
pitufogranjero <pitufogranjero@gmail.com>
2023-12-08 10:16:34 +0100

    refactor: separate UIChanger in two classes

--- src/userInterface/ILettersManager.ts
+++ src/userInterface/ILettersManager.ts
@@ -1,5 +1,8 @@
 export interface ILetterManagerInterface {
     letter: string;
+    turn: number;
+    position: number;
+    
     setNewLetter(turn: number, position: number, letter: string): void;
     deleteLetter(turn: number, position: number): void;
   }
