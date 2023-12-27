c95becb5597100c673f1a2eba1ebc039edd164b5
Mariajo <albiach33@gmail.com>
2023-12-27 11:45:46 +0100

    fix: add background color to repeating letters

--- src/userInterface/ILettersManager.ts
+++ src/userInterface/ILettersManager.ts
@@ -2,8 +2,6 @@ export interface ILetterManagerInterface {
     turn: number;
     position: number;
     letter: string;
-    turn: number;
-    position: number;
     
     setNewLetter(turn: number, position: number, letter: string): void;
     deleteLetter(turn: number, position: number): void;
