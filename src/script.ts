a58b5002e4c06eb36e58faeed0814466bb36c9a7
Mariajo <albiach33@gmail.com>
2024-01-03 09:37:05 +0100

    refactor: created IManageKeys

--- src/script.ts
+++ src/script.ts
@@ -1,26 +1,12 @@
-import {Word} from "./Word.js";
-import {Game} from "./Game.js";
-import {solutionWord} from "./solutionWord.js";
+import { ManageKeys } from "./manageKeys/ManageKeys.js";
 
-
-// const wordsCollection: Word = new Word(["JUEGO", "TALAR", "BAILE", "ANDAR", "MONTE", "PLAYA", "PLATA", "ARBOL", "QUESO"]);
-// const pickedWord: string = wordsCollection.getRandomWord();
-// console.log(pickedWord);
-
-async function startGame() {
-    const word = new solutionWord();
-    const pickedWord = await word.getRandomWordAPI();
-    console.log(pickedWord)
-
-    const game = new Game(pickedWord);
-
-    Array.from(document.getElementsByClassName("key")).forEach(element => element.addEventListener("click", (e)=>{
-        game.newKeyPressed((<HTMLButtonElement>e.target).value);
-    }));
-
-    document.addEventListener("keydown", (e)=>{
-        game.newKeyPressed(e.code);
-    });
-
+// Función principal para iniciar el juego
+async function startGame(): Promise<void> {
+    const manageKeysInstance = new ManageKeys();
+    const pickedWord: string = await manageKeysInstance.getRandomWord();
+    manageKeysInstance.printPickedWord(pickedWord);
+    manageKeysInstance.setupGameWithListeners(pickedWord);
 }
+
+// Iniciar el juego
 startGame();
